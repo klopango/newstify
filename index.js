74fb3cde78755bf0a48dc5ce68054fa90917ece9
Roger D. Stach <rogerdanistach@gmail.com>
2017-07-08 16:56:35 +0700

    Undoned feature

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ class StartController extends TelegramBaseController {
 class NewsController extends TelegramBaseController {
 
     askMedia($) {
-      $.sendMessage('List of media: \n - Buzzfeed - /buzzfeed\n - Daily Mail - /dailymail\n - MTV - /mtv\n - National Geographic - /natgeo\n - TechCrunch - /techcrunch\n - The New York Times - /newyork\n - The Next Web - /thenextweb\n - The Verge - /theverge')
+      $.sendMessage('List of media: \n - Buzzfeed - buzzfeed\n - Daily Mail - dailymail\n - MTV - mtv\n - National Geographic - natgeo\n - TechCrunch - techcrunch\n - The New York Times - newyork\n - The Next Web - thenextweb\n - The Verge - theverge')
       $.waitForRequest
           .then($ => {
             if ($.message.text == 'buzzfeed') {
@@ -74,7 +74,7 @@ class NewsController extends TelegramBaseController {
                     $.sendMessage(title_9 + ' - ' + url_9 + '\n');
                     $.sendMessage(title_10 + ' - ' + url_10 + '\n');
               });
-            } else if ($.message.text == 'dailymail'){
+            } else if ($.message.text == '!dailymail'){
               client.get("https://newsapi.org/v1/articles?source=daily-mail&sortBy=latest&apiKey=c95262ea5eed4b61b6cb31ac2f1567af", function (data) {
                 'use strict';
                 var url_1 = data.articles[0].url,
@@ -108,7 +108,7 @@ class NewsController extends TelegramBaseController {
                     $.sendMessage(title_9 + ' - ' + url_9 + '\n');
                     $.sendMessage(title_10 + ' - ' + url_10 + '\n');
               });
-            } else if ($.message.text == 'mtv'){
+            } else if ($.message.text == '!mtv'){
               client.get("https://newsapi.org/v1/articles?source=mtv-news&sortBy=latest&apiKey=c95262ea5eed4b61b6cb31ac2f1567af", function (data) {
                 'use strict';
                 var url_1 = data.articles[0].url,
@@ -142,7 +142,7 @@ class NewsController extends TelegramBaseController {
                     $.sendMessage(title_9 + ' - ' + url_9 + '\n');
                     $.sendMessage(title_10 + ' - ' + url_10 + '\n');
               });
-            } else if ($.message.text == 'natgeo'){
+            } else if ($.message.text == '!natgeo'){
               client.get("https://newsapi.org/v1/articles?source=national-geographic&sortBy=latest&apiKey=c95262ea5eed4b61b6cb31ac2f1567af", function (data) {
                 'use strict';
                 var url_1 = data.articles[0].url,
@@ -176,7 +176,7 @@ class NewsController extends TelegramBaseController {
                     $.sendMessage(title_9 + ' - ' + url_9 + '\n');
                     $.sendMessage(title_10 + ' - ' + url_10 + '\n');
               });
-            } else if ($.message.text == 'techcrunch'){
+            } else if ($.message.text == '!techcrunch'){
               client.get("https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=c95262ea5eed4b61b6cb31ac2f1567af", function (data) {
                 'use strict';
                 var url_1 = data.articles[0].url,
@@ -210,7 +210,7 @@ class NewsController extends TelegramBaseController {
                     $.sendMessage(title_9 + ' - ' + url_9 + '\n');
                     $.sendMessage(title_10 + ' - ' + url_10 + '\n');
               });
-            } else if ($.message.text == 'newyork'){
+            } else if ($.message.text == '!newyork'){
               client.get("https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=latest&apiKey=c95262ea5eed4b61b6cb31ac2f1567af", function (data) {
                 'use strict';
                 var url_1 = data.articles[0].url,
@@ -244,7 +244,7 @@ class NewsController extends TelegramBaseController {
                     $.sendMessage(title_9 + ' - ' + url_9 + '\n');
                     $.sendMessage(title_10 + ' - ' + url_10 + '\n');
               });
-            } else if ($.message.text == 'the-next-web'){
+            } else if ($.message.text == '!the-next-web'){
               client.get("https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=latest&apiKey=c95262ea5eed4b61b6cb31ac2f1567af", function (data) {
                 'use strict';
                 var url_1 = data.articles[0].url,
@@ -312,40 +312,6 @@ class NewsController extends TelegramBaseController {
                     $.sendMessage(title_9 + ' - ' + url_9 + '\n');
                     $.sendMessage(title_10 + ' - ' + url_10 + '\n');
               });
-            } else if ($.message.text == 'wired'){
-              client.get("https://newsapi.org/v1/articles?source=wired-de&sortBy=latest&apiKey=c95262ea5eed4b61b6cb31ac2f1567af", function (data) {
-                'use strict';
-                var url_1 = data.articles[0].url,
-                    url_2 = data.articles[1].url,
-                    url_3 = data.articles[2].url,
-                    url_4 = data.articles[3].url,
-                    url_5 = data.articles[4].url,
-                    url_6 = data.articles[5].url,
-                    url_7 = data.articles[6].url,
-                    url_8 = data.articles[7].url,
-                    url_9 = data.articles[8].url,
-                    url_10 = data.articles[9].url,
-                    title_1 = data.articles[0].title,
-                    title_2 = data.articles[1].title,
-                    title_3 = data.articles[2].title,
-                    title_4 = data.articles[3].title,
-                    title_5 = data.articles[4].title,
-                    title_6 = data.articles[5].title,
-                    title_7 = data.articles[6].title,
-                    title_8 = data.articles[7].title,
-                    title_9 = data.articles[8].title,
-                    title_10 = data.articles[9].title;
-                    $.sendMessage(title_1 + ' - ' + url_1 + '\n');
-                    $.sendMessage(title_2 + ' - ' + url_2 + '\n');
-                    $.sendMessage(title_3 + ' - ' + url_3 + '\n');
-                    $.sendMessage(title_4 + ' - ' + url_4 + '\n');
-                    $.sendMessage(title_5 + ' - ' + url_5 + '\n');
-                    $.sendMessage(title_6 + ' - ' + url_6 + '\n');
-                    $.sendMessage(title_7 + ' - ' + url_7 + '\n');
-                    $.sendMessage(title_8 + ' - ' + url_8 + '\n');
-                    $.sendMessage(title_9 + ' - ' + url_9 + '\n');
-                    $.sendMessage(title_10 + ' - ' + url_10 + '\n');
-              });
             } else {
               $.sendMessage(`Hi ${$.message.text}!`)
             }
@@ -359,6 +325,50 @@ class NewsController extends TelegramBaseController {
     }
 }
 
+class MediaController extends TelegramBaseController {
+  buzzfeedHandler($) {
+    client.get("https://newsapi.org/v1/articles?source=buzzfeed&sortBy=latest&apiKey=c95262ea5eed4b61b6cb31ac2f1567af", function (data) {              
+      'use strict';
+      var url_1 = data.articles[0].url,
+          url_2 = data.articles[1].url,
+          url_3 = data.articles[2].url,
+          url_4 = data.articles[3].url,
+          url_5 = data.articles[4].url,
+          url_6 = data.articles[5].url,
+          url_7 = data.articles[6].url,
+          url_8 = data.articles[7].url,
+          url_9 = data.articles[8].url,
+          url_10 = data.articles[9].url,
+          title_1 = data.articles[0].title,
+          title_2 = data.articles[1].title,
+          title_3 = data.articles[2].title,
+          title_4 = data.articles[3].title,
+          title_5 = data.articles[4].title,
+          title_6 = data.articles[5].title,
+          title_7 = data.articles[6].title,
+          title_8 = data.articles[7].title,
+          title_9 = data.articles[8].title,
+          title_10 = data.articles[9].title;
+          $.sendMessage(title_1 + ' - ' + url_1 + '\n');
+          $.sendMessage(title_2 + ' - ' + url_2 + '\n');
+          $.sendMessage(title_3 + ' - ' + url_3 + '\n');
+          $.sendMessage(title_4 + ' - ' + url_4 + '\n');
+          $.sendMessage(title_5 + ' - ' + url_5 + '\n');
+          $.sendMessage(title_6 + ' - ' + url_6 + '\n');
+          $.sendMessage(title_7 + ' - ' + url_7 + '\n');
+          $.sendMessage(title_8 + ' - ' + url_8 + '\n');
+          $.sendMessage(title_9 + ' - ' + url_9 + '\n');
+          $.sendMessage(title_10 + ' - ' + url_10 + '\n');
+    });
+  }
+
+  get routes() {
+    return {
+      'buzzfeed' : 'buzzfeedHandler'
+    }
+  }
+}
+
 class OtherwiseController extends TelegramBaseController {
     handle($) {
         $.sendMessage('That command doesn\'t even exist, are you high?')
@@ -369,4 +379,5 @@ tg.router
     .when(new TextCommand('/start', 'startCommand'), new StartController())
     .when(new TextCommand('/help', 'helpCommand'), new StartController())
     .when(new TextCommand('/news', 'newsCommand'), new NewsController())
+    .when(new TextCommand('/buzzfeed', 'buzzfeed'), new MediaController())
     .otherwise(new OtherwiseController())
